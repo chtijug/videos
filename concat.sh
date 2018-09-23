7b87de6d48850b410b908afd0bfaa08c28a537cb
Guillaume Wallet <wallet.guillaume@gmail.com>
2018-09-23 12:57:42 +0200

    ADD concat computer video parts and 2018-09 session

--- concat.sh
+++ concat.sh
@@ -2,8 +2,22 @@
 
 TARGET=${1:-speaker}
 
-rm ${TARGET}.txt || true
-for part in `ls -1 PIC*.MP4`; do
-    echo "file '$part'" >> ${TARGET}.txt
-done
-ffmpeg -f concat -safe 0 -i ${TARGET}.txt -c copy ${TARGET}.mp4
+case $TARGET in
+    speaker)
+        rm ${TARGET}.txt || true
+        for part in `ls -1 PIC*.MP4`; do
+            echo "file '$part'" >> ${TARGET}.txt
+        done
+        ffmpeg -f concat -safe 0 -i ${TARGET}.txt -c copy ${TARGET}.mp4
+        ;;
+    computer)
+        rm ${TARGET}.txt || true
+        for part in `ls -1 VGA_*.mov`; do
+            echo "file '$part'" >> ${TARGET}.txt
+        done
+        ffmpeg -f concat -safe 0 -i ${TARGET}.txt -c copy ${TARGET}.mp4
+        ;;
+    *)
+        exit 1
+        ;;
+esac
